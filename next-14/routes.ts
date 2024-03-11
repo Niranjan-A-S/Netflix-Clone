8ef385d193135cd86d13338ab448716ef5f80e36
Niranjan Avanippilli Santhosh <niranjan.santhosh@agilepoint.com>
2024-03-11 20:43:01 +0530

    chore: added  routes file

---
+++ next-14/routes.ts
@@ -0,0 +1,11 @@
+export const authRoutes: string[] = [
+    '/auth'
+];
+
+export const publicRoutes: string[] = [
+    '/test'
+];
+
+export const apiAuthPrefix: string = '/api/auth';
+
+export const DEFAULT_LOGIN_REDIRECT: string = '/';
