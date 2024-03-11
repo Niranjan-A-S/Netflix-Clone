fa6dfc37230a34134c06065d6fc66c29fa76a39b
Niranjan Avanippilli Santhosh <niranjan.santhosh@agilepoint.com>
2024-03-11 15:50:19 +0530

    chore: global css reset

--- next-14/app/layout.tsx
+++ next-14/app/layout.tsx
@@ -1,16 +1,16 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Netflix India | Watch TV Shows Online, Watch Movies Online",
-  description: "Developed by Niranjan A S",
+  title: 'Netflix India | Watch TV Shows Online, Watch Movies Online',
+  description: 'Developed by Niranjan A S'
 };
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
