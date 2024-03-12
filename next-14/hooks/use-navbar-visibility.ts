d4a1056bfe403bf479864a65ccf0020327d94d2a
Niranjan Avanippilli Santhosh <niranjan.santhosh@agilepoint.com>
2024-03-12 16:14:26 +0530

    feat: completed navbar functionality

---
+++ next-14/hooks/use-navbar-visibility.ts
@@ -0,0 +1,36 @@
+import { TOP_OFFSET } from "@/constants";
+import { useState, useEffect, useCallback } from "react";
+
+export const useNavbarVisibility = () => {
+    const [showAccountMenu, setShowAccountMenu] = useState(false);
+    const [showMobileMenu, setShowMobileMenu] = useState(false);
+    const [showBackground, setShowBackground] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY >= TOP_OFFSET) {
+                setShowBackground(true);
+            } else {
+                setShowBackground(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    const toggleAccountMenu = useCallback(() => {
+        setShowAccountMenu((current) => !current);
+    }, []);
+
+    const toggleMobileMenu = useCallback(() => {
+        setShowMobileMenu((current) => !current);
+    }, []);
+
+    return {
+        showAccountMenu, showMobileMenu, showBackground, toggleAccountMenu, toggleMobileMenu
+    }
+}
