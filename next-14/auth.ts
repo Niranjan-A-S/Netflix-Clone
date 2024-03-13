2976311929ea6e5202dbc1d95562bb7222eb45bf
Niranjan Avanippilli Santhosh <niranjan.santhosh@agilepoint.com>
2024-03-13 20:36:23 +0530

    chore: extended  the session object to have id

--- next-14/auth.ts
+++ next-14/auth.ts
@@ -7,6 +7,14 @@ export const {
     handlers: { GET, POST },
     auth, signIn, signOut
 } = NextAuth({
+    callbacks: {
+        async session({ session, token }) {
+            if (token.sub && session.user) {
+                session.user.id = token.sub;
+            }
+            return session;
+        }
+    },
     adapter: PrismaAdapter(db),
     session: { strategy: 'jwt' },
     ...authConfig
