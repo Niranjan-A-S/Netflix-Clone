80d85b585ebe10dd208695bc1af6a44da85e94fc
Niranjan Avanippilli Santhosh <niranjan.santhosh@agilepoint.com>
2024-03-14 17:11:10 +0530

    feat: added api route for random movie

---
+++ next-14/app/api/random/route.ts
@@ -0,0 +1,18 @@
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export const GET = async (req: Request) => {
+    try {
+        const moviesCount = await db.movie.count();
+        const randomIndex = Math.floor(Math.random() * moviesCount);
+
+        const randomMovies = await db.movie.findMany({
+            take: 1,
+            skip: randomIndex
+        });
+
+        return NextResponse.json(randomMovies[0])
+    } catch (error) {
+        return new NextResponse('Internal Error', { status: 500 })
+    }
+}
