d8ab9bc3e9204791673c4438c3110103cd472c48
Niranjan Avanippilli Santhosh <niranjan.santhosh@agilepoint.com>
2024-03-15 11:02:48 +0530

    chore: modified the use movie hook

--- hooks/use-movie.ts
+++ hooks/use-movie.ts
@@ -1,16 +1,16 @@
-import { fetcher } from "@/lib/utils"
-import useSWR from "swr"
+import { fetcher } from '@/lib/utils';
+import useSWR from 'swr';
 
-export const useMovie = (movieId: string) => {
-    const { data, error, isLoading } = useSWR(`/api/movie/${movieId}`, fetcher, {
+export const useMovie = (movieId?: string) => {
+    const { data, error, isLoading } = useSWR(movieId ? `/api/movie/${movieId}` : null, fetcher, {
         revalidateIfStale: false,
         revalidateOnFocus: false,
-        revalidateOnReconnect: false,
+        revalidateOnReconnect: false
     });
 
     return {
         data,
         error,
         isLoading
-    }
-}
+    };
+};
