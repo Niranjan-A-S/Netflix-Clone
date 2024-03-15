b6e7d75ddeda6796b3beee2e3f8baf04092d98f3
Niranjan Avanippilli Santhosh <niranjan.santhosh@agilepoint.com>
2024-03-15 13:00:40 +0530

    feat: integrated the favorited list of movies to home page

--- app/(protected)/page.tsx
+++ app/(protected)/page.tsx
@@ -1,16 +1,17 @@
 import { BillBoard } from '@/components/home/bill-board';
+import { FavoriteList } from '@/components/home/favorite-list';
 import { InfoModal } from '@/components/home/info-modal';
 import { TrendingList } from '@/components/home/trending-list';
 import { Navbar } from '@/components/navbar';
 
 export default async function HomePage() {
-
     return <>
         <InfoModal />
         <Navbar />
         <BillBoard />
         <div className="pb-40">
             <TrendingList />
+            <FavoriteList />
         </div>
     </>;
 }
--- components/home/favorite-list.tsx
+++ components/home/favorite-list.tsx
@@ -1,11 +1,11 @@
 'use client';
 
-import { useMovieList } from '@/hooks/use-movie-list';
+import { useFavorites } from '@/hooks/use-favorites';
 import React, { memo } from 'react';
 import { MovieList } from './movie-list';
 
 export const FavoriteList: React.FC = memo(() => {
-    const { data: movies = [], error } = useMovieList();
+    const { data: movies = [], error } = useFavorites();
     return <MovieList data={movies} title="Favorites" hasError={!!error} />;
 });
 
