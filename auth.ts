047010e3653c9b72adae8f5617e30576d77a0e88
Niranjan Avanippilli Santhosh <niranjan.santhosh@agilepoint.com>
2024-03-15 13:02:11 +0530

    chore: extended session props

--- auth.ts
+++ auth.ts
@@ -11,6 +11,12 @@ export const {
         async session({ session, token }) {
             if (token.sub && session.user) {
                 session.user.id = token.sub;
+                const user = await db.user.findUnique({
+                    where: {
+                        id: token.sub
+                    }
+                });
+                session.user.favoriteIds = user?.favoriteIds;
             }
             return session;
         }
