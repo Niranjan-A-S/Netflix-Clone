855cb9366dbf823d48713375296e59de8ec7fb3d
Niranjan Avanippilli Santhosh <niranjan.santhosh@agilepoint.com>
2024-03-15 14:22:51 +0530

    feat: added API route to handle update and delete methods of favorite movies

--- app/api/movies/favorites/route.ts
+++ app/api/movies/favorites/route.ts
@@ -5,9 +5,7 @@ import { NextResponse } from 'next/server';
 export const GET = async (req: Request) => {
     try {
         const session = await auth();
-        if (!session || !session.user) {
-            return new NextResponse('Unauthorized', { status: 401 });
-        }
+        if (!session || !session.user) return new NextResponse('Unauthorized', { status: 401 });
 
         const favoriteMovies = await db.movie.findMany({
             where: {
@@ -22,4 +20,74 @@ export const GET = async (req: Request) => {
         return new NextResponse('Internal Error', { status: 500 });
     }
 
+};
+
+export const POST = async (req: Request) => {
+    try {
+
+        const session = await auth();
+        if (!session || !session.user) return new NextResponse('Unauthorized', { status: 401 });
+
+        const { movieId } = await req.json();
+        if (!movieId) return new NextResponse('No movie ID found', { status: 422 });
+
+        const existingMovie = await db.movie.findUnique({
+            where: {
+                id: movieId
+            }
+        });
+        if (!existingMovie) return new NextResponse('No movie with the provided ID exist'), { status: 404 };
+
+        const user = await db.user.update({
+            where: {
+                id: session.user.id
+            },
+            data: {
+                favoriteIds: {
+                    push: [
+                        movieId
+                    ]
+                }
+            }
+        });
+
+        return NextResponse.json(user);
+
+    } catch (error) {
+        return new NextResponse('Internal Error!', { status: 500 });
+    }
+};
+
+export const DELETE = async (req: Request) => {
+    try {
+
+        const session = await auth();
+        if (!session || !session.user) return new NextResponse('Unauthorized', { status: 401 });
+
+        const { movieId } = await req.json();
+        if (!movieId) return new NextResponse('No movie ID found', { status: 422 });
+
+        const existingMovie = await db.movie.findUnique({
+            where: {
+                id: movieId
+            }
+        });
+        if (!existingMovie) return new NextResponse('No movie with the provided ID exist'), { status: 404 };
+
+        const user = await db.user.update({
+            where: {
+                id: session.user.id
+            },
+            data: {
+                favoriteIds: {
+                    set: session.user.favoriteIds?.filter((id) => id !== movieId)
+                }
+            }
+        });
+
+        return NextResponse.json(user);
+
+    } catch (error) {
+        return new NextResponse('Internal Error!', { status: 500 });
+    }
 };
