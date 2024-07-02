a128a5413a33dbceab214efb57c1da0755530a21
Niranjan A S <niranjan0881@gmail.com>
2024-07-02 12:55:18 +0530

    Revert "chore: cached the movies query"
    This reverts commit 53816d8c78733f1e26794a9d527ac51a389e85b7.

--- app/api/movies/route.ts
+++ app/api/movies/route.ts
@@ -1,14 +1,9 @@
 import { db } from '@/lib/db';
-import redis, { MOVIES_KEY } from '@/redis';
 import { NextResponse } from 'next/server';
 
 export const GET = async (_req: Request) => {
     try {
-        const cachedMovies = await redis?.get(MOVIES_KEY);
-        if (cachedMovies) return NextResponse.json(JSON.parse(cachedMovies));
-
         const movies = await db.movie.findMany({});
-        await redis?.set(MOVIES_KEY, JSON.stringify(movies));
 
         return NextResponse.json(movies);
     } catch (error) {
